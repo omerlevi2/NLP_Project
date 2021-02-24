33452ae217786318e26a683fb9a3a1fa32de6484
Glickman <glickman1@mail.tau.ac.il>
2021-02-24 16:50:57 +0200

    fix encoding

--- scripts/join_startegyqa_evidence.py
+++ scripts/join_startegyqa_evidence.py
@@ -1,7 +1,6 @@
 import json
 
 
-
 def get_evidence(startqa_example):
     def get_evidence_strings(evidence):
         if isinstance(evidence, str):
@@ -15,23 +14,12 @@ def get_evidence(startqa_example):
     evidence_ids = get_evidence_strings(evidence)
     return [x for x in evidence_ids if not x == 'operation' and not x == 'no_evidence']
 
+
 paragraphs_json = '../data/strategyqa/strategyqa_train_paragraphs.json'
 train_json = '../data/strategyqa/train.json'
 with open(paragraphs_json, 'r') as para_file:
     paragraphs = json.load(para_file)
-with open(train_json, 'r') as train_file:
+with open(train_json, 'r',encoding="utf8") as train_file:
     questions = json.load(train_file)
-    
+
 x = get_evidence(questions[0])
-
-
-
-
-
-
-
-
-
-
-
-
