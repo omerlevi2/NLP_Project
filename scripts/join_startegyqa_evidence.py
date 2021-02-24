7b28737ad5d27b70c4117c751a84aa360aaee515
Glickman <glickman1@mail.tau.ac.il>
2021-02-24 16:06:58 +0200

    get evidence ids

--- scripts/join_startegyqa_evidence.py
+++ scripts/join_startegyqa_evidence.py
@@ -10,16 +10,18 @@ with open(train_json, 'r') as train_file:
 
 
 def get_evidence(startqa_example):
-    def get_evidence_ids(evidence):
+    def get_evidence_strings(evidence):
         if isinstance(evidence, str):
-            if evidence == 'operation' or evidence == 'no_evidence':
-                return []
-            else:
-                return [evidence]
+            return [evidence]
         ids = []
         for evi in evidence:
-            ids.extend(get_evidence_ids(evi))
+            ids.extend(get_evidence_strings(evi))
         return ids
+
+    evidence = startqa_example['evidence']
+    evidence_ids = get_evidence_strings(evidence)
+    return [x for x in evidence_ids if not x == 'operation' and not x == 'no_evidence']
+
 
 print(paragraphs)
 
