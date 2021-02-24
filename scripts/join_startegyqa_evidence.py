178e274bfd703e89de679e3a2e7065d4217fe790
Glickman <glickman1@mail.tau.ac.il>
2021-02-24 16:01:48 +0200

    get evidence ids

--- scripts/join_startegyqa_evidence.py
+++ scripts/join_startegyqa_evidence.py
@@ -1,4 +1,5 @@
 import json
+from typing import List, Dict
 
 paragraphs_json = '../data/strategyqa/strategyqa_train_paragraphs.json'
 train_json = '../data/strategyqa/train.json'
@@ -6,6 +7,19 @@ with open(paragraphs_json, 'r') as para_file:
     paragraphs = json.load(para_file)
 with open(train_json, 'r') as train_file:
     questions = json.load(train_file)
+
+
+def get_evidence(startqa_example):
+    def get_evidence_ids(evidence):
+        if isinstance(evidence, str):
+            if evidence == 'operation' or evidence == 'no_evidence':
+                return []
+            else:
+                return [evidence]
+        ids = []
+        for evi in evidence:
+            ids.extend(get_evidence_ids(evi))
+        return ids
 
 print(paragraphs)
 
