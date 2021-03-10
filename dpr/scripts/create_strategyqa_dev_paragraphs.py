ab8d5f7d7c27f807d8d2ca3ddb6463e6295e0ed1
Glickman <glickman1@mail.tau.ac.il>
2021-03-10 10:08:06 +0200

    minor

--- dpr/scripts/create_strategyqa_dev_paragraphs.py
+++ dpr/scripts/create_strategyqa_dev_paragraphs.py
@@ -2,10 +2,15 @@ import json
 
 from dpr.scripts.utils import get_evidence_ids
 
-split = 'dev'
 
-train_json = '../../data/strategyqa/%s.json' % split
+def get_all_evidence_ids():
+    global split
+    split = 'dev'
+    train_json = '../../data/strategyqa/%s.json' % split
+    with open(train_json, 'r', encoding="utf8") as train_file:
+        questions = json.load(train_file)
+        ids = sum([get_evidence_ids(q) for q in questions], [])
+        return set(ids)
 
-with open(train_json, 'r', encoding="utf8") as train_file:
-    questions = json.load(train_file)
-    all_evidence_ids = set(sum([get_evidence_ids(q) for q in questions], []))
+
+all_evidence_ids = get_all_evidence_ids()
