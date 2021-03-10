1286c94378c86b8a76cee8b96293e5a8d2a75588
Glickman <glickman1@mail.tau.ac.il>
2021-03-10 15:59:52 +0200

    train method

--- dpr/experiments/retriever.py
+++ dpr/experiments/retriever.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 from haystack.retriever.dense import DensePassageRetriever
 
 batch_size: int = 1024
@@ -33,4 +34,38 @@ def load_retriever(document_store):
         use_gpu=True,
         batch_size=batch_size,
         embed_title=True
+    )
+
+
+@dataclass
+class RetrieverTrainParams:
+    data_dir: str = '../../data/'
+    save_dir: str = '/saved_models'
+
+    train_filename: str = 'train_parsed.json'
+    dev_filename: str = '../../data/dev_parsed.json'
+    test_filename: str = dev_filename
+
+    n_epochs: int = 1
+    batch_size: int = 4
+    grad_acc_steps: int = 4
+
+    num_positives = 1
+    num_hard_negatives = 1
+
+
+def train(retriever, params: RetrieverTrainParams):
+    retriever.train(
+        data_dir=params.data_dir,
+        train_filename=params.train_filename,
+        dev_filename=params.dev_filename,
+        test_filename=params.dev_filename,
+        n_epochs=params.n_epochs,
+        batch_size=params.batch_size,
+        grad_acc_steps=params.grad_acc_steps,
+        save_dir=params.save_dir,
+        evaluate_every=3000,
+        embed_title=True,
+        num_positives=params.num_positives,
+        num_hard_negatives=params.num_hard_negatives
     )
