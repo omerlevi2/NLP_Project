97b832fd43b08e5ae77de5b0b7dd74ceba3bfcdf
Glickman <glickman1@mail.tau.ac.il>
2021-03-16 18:54:00 +0200

    updating embedding after training

--- dpr/experiments/test_training_retriever.py
+++ dpr/experiments/test_training_retriever.py
@@ -10,15 +10,14 @@ formated_file_name = doc_dir + 'startqa_corpus_formatted_for_documentstore.json'
 # formated_file_name = doc_dir + 'sample_startqa_corpus_formatted_for_documentstore.jsonl'
 
 if load:
-    ds = document_store.get_faiss_document_store()
+    ds = document_store.load_saved_document_store()
     retriever = retrieves.load_retriever(ds)
 else:
     # here possibliy create from scratch.
     ds = document_store.get_faiss_document_store()
     retriever = retrieves.get_retriever_for_training()
-    trainer.train(retriever,
-                  trainer.RetrieverTrainParams(num_hard_negatives=0, n_epochs=1))
     update_document_store_embeddings_and_save(ds, retriever)
+    ds = document_store.load_saved_document_store()
 
 res = retriever.retrieve('West')
 print(res)
