fcea39c1524b62ae097eac4e161a88229ae81621
Glickman <glickman1@mail.tau.ac.il>
2021-03-16 17:54:11 +0200

    document

--- dpr/experiments/test_training_retriever.py
+++ dpr/experiments/test_training_retriever.py
@@ -1,5 +1,6 @@
 import dpr.retrievers.retrieves as retrieves
 import dpr.retrievers.trainer as trainer
+import document_store
 
 load = True
 
@@ -8,10 +9,13 @@ formated_file_name = doc_dir + 'startqa_corpus_formatted_for_documentstore.json'
 # formated_file_name = doc_dir + 'sample_startqa_corpus_formatted_for_documentstore.jsonl'
 
 if load:
-    retriever = retrieves.load_retriever()
+    document_store = document_store.get_faiss_document_store()
+    retriever = retrieves.load_retriever(document_store)
 else:
     retriever = retrieves.get_retriever_for_training()
     trainer.train(retriever,
                   trainer.RetrieverTrainParams(num_hard_negatives=0, n_epochs=1))
 
+res = retriever.retrieve('Does Jerry Seinfeld hang out at the Budweiser Party Deck')
+print(res)
 print(3)
--- dpr/progress.md
+++ dpr/progress.md
@@ -10,15 +10,11 @@ what is this? DPR model loaded from ret_save_file
 
 how many epochs should dpr be trained for?
 
-train retriver on startegyqa dataset
-
-fix dev set paragraphs:
-join_startegyqa_evidence.py does not work because there is no strategyqa_dev_paragraphs.json
 
 # this should be a larger number.see comment on notion on uneven number of positives
 num_positives:int = 1
+deal with hard_negative_ctxs... find a reasonable number
 
-deal with hard_negative_ctxs
 
 done:
 reformat files strategyQA files to include actual evidence moved all our stuff to dpr package
