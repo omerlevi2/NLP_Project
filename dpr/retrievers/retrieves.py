e7bc2155a0950b4a8724a9fb0e34337c87bedb7c
Glickman <glickman1@mail.tau.ac.il>
2021-03-17 18:43:01 +0200

    use gpu only when avialable

--- dpr/retrievers/retrieves.py
+++ dpr/retrievers/retrieves.py
@@ -1,5 +1,6 @@
 from haystack.document_store.memory import InMemoryDocumentStore
 from haystack.retriever.dense import DensePassageRetriever
+import torch
 
 batch_size: int = 1024
 retriever_save_path = 'ret_save_file'
@@ -7,14 +8,15 @@ retriever_save_path = 'ret_save_file'
 
 def get_retriever(document_store,
                   query_model="facebook/dpr-question_encoder-single-nq-base",
-                  passage_model="facebook/dpr-ctx_encoder-single-nq-base"):
+                  passage_model="facebook/dpr-ctx_encoder-single-nq-base",
+                  batch_size=128):
     return DensePassageRetriever(
         document_store=document_store,
         query_embedding_model=query_model,
         passage_embedding_model=passage_model,
         max_seq_len_query=64,
         max_seq_len_passage=256,
-        use_gpu=True,
+        use_gpu=torch.cuda.is_available(),
         batch_size=batch_size,
         embed_title=True
     )
