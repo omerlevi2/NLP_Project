9852d209bedaffabb8e639e0202658278d2a5706
Glickman <glickman1@mail.tau.ac.il>
2021-03-17 18:42:24 +0200

    class for StrategyQAWikiCorpus

--- dpr/retrievers/corpus/StrategyQAWikiCorpus.py
+++ dpr/retrievers/corpus/StrategyQAWikiCorpus.py
@@ -3,5 +3,8 @@ from haystack.preprocessor.utils import fetch_archive_from_http
 
 class StrategyQAWikiCorpus:
     def __init__(self):
-        s3_url_dev = 'link_to_my_zip_startqa_corpus_formatted_for_documentstore.zip'
-        fetch_archive_from_http(s3_url_dev, output_dir='corpus/')
+        s3_url_dev = 'https://dpr-nlp.s3.amazonaws.com/startqa_corpus_formatted_for_documentstore.zip'
+        fetch_archive_from_http(s3_url_dev, output_dir='corpus/stratCorpus')
+
+    def filepath(self):
+        return 'corpus/stratCorpus/startqa_corpus_formatted_for_documentstore.json'
