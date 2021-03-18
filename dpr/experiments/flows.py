29c76b048060aed09af06696087c1568152152c1
Glickman <glickman1@mail.tau.ac.il>
2021-03-18 11:16:36 +0200

    util for creating db

--- dpr/experiments/flows.py
+++ dpr/experiments/flows.py
@@ -1,5 +1,8 @@
+from dpr.experiments import document_store
 from dpr.experiments.document_store import get_faiss_document_store, populate_document_store_from_strategyqa, \
     save_document_store
+# from dpr.experiments.loading_nq_flow import document_store
+from dpr.retrievers.corpus.StrategyQAWikiCorpus import StrategyQAWikiCorpus
 from dpr.retrievers.retrieves import get_retriever, save_retriever
 
 
@@ -19,3 +22,13 @@ def update_document_store_embeddings_and_save(document_store, retriever):
     document_store.update_embeddings(retriever)
     save_document_store(document_store)
     save_retriever(retriever)
+
+
+def create_faiss_db_on_stratqa_corpus():
+    ds = document_store.get_faiss_document_store()
+    print('document count:', ds.get_document_count())
+    if ds.get_document_count() == 0:
+        path = StrategyQAWikiCorpus().filepath()
+        document_store.populate_document_store_from_strategyqa(path, ds)
+
+    return ds
