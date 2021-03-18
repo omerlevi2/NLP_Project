2540ab320b20a7e7a2308adb36a581c085aa7f4f
Glickman <glickman1@mail.tau.ac.il>
2021-03-18 16:10:48 +0200

    doucment stuff

--- dpr/experiments/hyperparams.py
+++ dpr/experiments/hyperparams.py
@@ -1,1 +1,3 @@
-faiss_index_factory_str = "Flat"
+# faiss_index_factory_str = "Flat"
+#60000 = sqrt(len(corpus)) * 10 . see here https://haystack.deepset.ai/docs/latest/apidatabasemd
+faiss_index_factory_str = "IVF60000,Flat"
