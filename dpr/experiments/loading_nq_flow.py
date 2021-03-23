db7f09c6ec29dd3cb6687bfb9fb3d87641aa6809
Glickman <glickman1@mail.tau.ac.il>
2021-03-23 15:15:32 +0200

    change example to work with elastic

--- dpr/experiments/loading_nq_flow.py
+++ dpr/experiments/loading_nq_flow.py
@@ -1,21 +1,26 @@
 from dpr.experiments.document_store import populate_document_store_from_strategyqa
-from haystack.document_store.sql import SQLDocumentStore
-from haystack.retriever.sparse import TfidfRetriever
+import dpr.experiments.document_store as document_store_utils
+import time
+from haystack.retriever.sparse import TfidfRetriever, ElasticsearchRetriever
 
 from dpr.retrievers.corpus.StrategyQAWikiCorpus import StrategyQAWikiCorpus
 from dpr.retrievers.dataset.NQDataset import NQDataset
 
 formated_file_name = StrategyQAWikiCorpus().filepath()
 
-document_store = SQLDocumentStore(url="sqlite:///qa.db")
+# document_store = SQLDocumentStore(url="sqlite:///qa.db")
+document_store = document_store_utils.get_elastic_document_store()
 print(document_store.get_document_count())
 if document_store.get_document_count() == 0:
     populate_document_store_from_strategyqa(formated_file_name, document_store)
 
-retriever = TfidfRetriever(document_store=document_store)
+print('initing tfidretriver')
+retriever = ElasticsearchRetriever(document_store=document_store)
 for sample in NQDataset().train_set():
     question_ = sample['question']
     print(question_)
     # print(sample['positive_ctxs'][0]['text'])
+    start = time.time()
     retrieve = retriever.retrieve(question_, top_k=1)
+    print('took', time.time() - start)
     print(retrieve)
