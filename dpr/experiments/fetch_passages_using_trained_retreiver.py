71b0e076280f490c0e461930414348846900c9aa
Glickman <glickman1@mail.tau.ac.il>
2021-04-06 17:07:21 +0300

    implement my own fetch

--- dpr/experiments/fetch_passages_using_trained_retreiver.py
+++ dpr/experiments/fetch_passages_using_trained_retreiver.py
@@ -3,19 +3,40 @@ import json
 from dpr.experiments import document_store
 from dpr.retrievers import retrieves
 from dpr.retrievers.dataset.StrategyQADataset import StrategyQADataset
+import torch
 
 qa_dataset = StrategyQADataset()
 ds = document_store.get_elastic_document_store()
 retriever = retrieves.load_retriever(ds, 'ret_save_file_nq_then_strat')
 
+embeded = retriever.embed_queries(['Who shot yeah'])
+
+docs = ds.get_all_documents()
+passage_embeddings = torch.tensor([doc.embedding for doc in docs])
+print(ds.query('Who are you'))
+
+passage_embeddings = torch.tensor([doc.embedding for doc in docs])  # shape(n_passage,d)
+questions = [x['question'] for x in qa_dataset.train_set()] + [x['question'] for x in qa_dataset.dev_set()]
+question_embeddings = retriever.embed_queries(questions).T  # shape (d,n_questions)
+result = torch.matmul(passage_embeddings, question_embeddings)  # shape (n_passage,n_questions
+# top_k indexes at column x are indexes in docs for questions[x]
+values, indices, = torch.topk(result, k=7, dim=0)
+print(indices)
+
+
+# _,top_k = torch.topk(result,k=7)
+# print(result)
 
 def retrieve_passages(dataset, d):
     for example in dataset:
         question = example['question']
-        retrieved = retriever.retrieve(question, top_k=7)
+        embeded = retriever.embed_queries([question])
+        # embeded = embeded[0]
+        # retrieved = retriever.retrieve(question, top_k=7)
         # passages = [x.text for x in retrieved]
+        scores = passage_embeddings * embeded
         print(example)
-        d[example] = retrieved
+        # d[example] = retrieved
 
 
 result = {}
@@ -25,4 +46,3 @@ print(result)
 
 with open('strategy_retrieved_passages.json', 'w', encoding='utf-8') as f:
     json.dump(result, f)
-
