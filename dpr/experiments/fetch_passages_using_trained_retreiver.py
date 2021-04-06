e0fa6251d4018de32fbf9731d4a0ed3592ba9efe
Glickman <glickman1@mail.tau.ac.il>
2021-04-06 22:00:05 +0300

    upload retrieved passages!

--- dpr/experiments/fetch_passages_using_trained_retreiver.py
+++ dpr/experiments/fetch_passages_using_trained_retreiver.py
@@ -1,5 +1,7 @@
 import json
 
+from tqdm import tqdm
+
 from dpr.experiments import document_store
 from dpr.retrievers import retrieves
 from dpr.retrievers.dataset.StrategyQADataset import StrategyQADataset
@@ -9,34 +11,43 @@ qa_dataset = StrategyQADataset()
 ds = document_store.get_elastic_document_store()
 retriever = retrieves.load_retriever(ds, 'ret_save_file_nq_then_strat')
 
-embeded = retriever.embed_queries(['Who shot yeah'])
 
-docs = ds.get_all_documents()
-passage_embeddings = torch.tensor([doc.embedding for doc in docs])
-print(ds.query('Who are you'))
-
-passage_embeddings = torch.tensor([doc.embedding for doc in docs])  # shape(n_passage,d)
-questions = [x['question'] for x in qa_dataset.train_set()] + [x['question'] for x in qa_dataset.dev_set()]
-question_embeddings = retriever.embed_queries(questions).T  # shape (d,n_questions)
-result = torch.matmul(passage_embeddings, question_embeddings)  # shape (n_passage,n_questions
-# top_k indexes at column x are indexes in docs for questions[x]
-values, indices, = torch.topk(result, k=7, dim=0)
-print(indices)
+# embeded = retriever.embed_queries(['Who shot yeah'])
+#
+# docs = ds.get_all_documents()
+# passage_embeddings = torch.tensor([doc.embedding for doc in docs])
+# print(ds.query('Who are you'))
+#
+# passage_embeddings = torch.tensor([doc.embedding for doc in docs])  # shape(n_passage,d)
+# questions = [x['question'] for x in qa_dataset.train_set()] + [x['question'] for x in qa_dataset.dev_set()]
+# question_embeddings = retriever.embed_queries(questions).T  # shape (d,n_questions)
+# result = torch.matmul(passage_embeddings, question_embeddings)  # shape (n_passage,n_questions
+# # top_k indexes at column x are indexes in docs for questions[x]
+# values, indices, = torch.topk(result, k=7, dim=0)
+# print(indices)
 
 
 # _,top_k = torch.topk(result,k=7)
 # print(result)
 
 def retrieve_passages(dataset, d):
-    for example in dataset:
+    for example in tqdm(dataset):
         question = example['question']
-        embeded = retriever.embed_queries([question])
+        # embeded = retriever.embed_queries([question])
         # embeded = embeded[0]
-        # retrieved = retriever.retrieve(question, top_k=7)
+        retrieved = retriever.retrieve(question, top_k=7)
+        ans = []
+        for ret in retrieved:
+            ret_dict = ret.__dict__
+            ret_dict.pop('embedding', None)
+            ans.append(ret_dict)
         # passages = [x.text for x in retrieved]
-        scores = passage_embeddings * embeded
-        print(example)
-        # d[example] = retrieved
+        # scores = passage_embeddings * embeded
+        print(question)
+        print(ans)
+        print('\n\n')
+        d[question] = ans
+        # break
 
 
 result = {}
--- dpr/experiments/test_training_retriever.py
+++ dpr/experiments/test_training_retriever.py
@@ -1,3 +1,5 @@
+import json
+
 import dpr.retrievers.retrieves as retrieves
 import dpr.retrievers.trainer as trainer
 from dpr.retrievers.dataset.NQDataset import NQDataset
@@ -8,8 +10,9 @@ from dpr.experiments.flows import update_document_store_embeddings_and_save
 
 load = False
 populate_doucment_store = False
-train = True
-update_embeddings = False
+train = False
+update_embeddings = True
+answer_flow = True
 
 doc_dir = 'data/'
 formated_file_name = doc_dir + 'startqa_corpus_formatted_for_documentstore.json'
@@ -20,22 +23,50 @@ if load:
     ds = document_store.load_saved_document_store()
     retriever = retrieves.load_retriever(ds)
 else:
-    # ds = document_store.get_elastic_document_store()
-    # print('document count:', ds.get_document_count())
+    ds = document_store.get_elastic_document_store()
+    print('document count:', ds.get_document_count())
     if train:
+
         qa_dataset = StrategyQADataset()
         # qa_dataset = NQDataset()
-        retriever = retrieves.get_retriever_for_training()
+        # retriever = retrieves.get_retriever_for_training()
+        retriever = retrieves.load_retriever(ds, 'ret_save_file_nq_only')
+        batch_size = 6
         trainer.train(retriever,
                       qa_dataset,
-                      RetrieverTrainParams(num_positives=1, num_hard_negatives=1, n_epochs=1,
-                                           batch_size=16,
-                                           grad_acc_steps=8, evaluate_every=100), save=True)
+                      RetrieverTrainParams(num_positives=1, num_hard_negatives=5, n_epochs=2,
+                                           batch_size=batch_size,
+                                           grad_acc_steps=16 // batch_size, evaluate_every=500 // batch_size)
+                      # RetrieverTrainParams(num_positives=1*4, num_hard_negatives=15, n_epochs=6,
+                      #                      batch_size=16*4,
+                      #                      grad_acc_steps=4, evaluate_every=100)
+                      , save=True)
     else:
-        retriever = retrieves.load_retriever(ds)
+        retriever = retrieves.load_retriever(ds,'ret_save_file_nq_then_strat')
     if update_embeddings:
         update_document_store_embeddings_and_save(ds, retriever)
     # ds = document_store.load_saved_document_store()
+    if answer_flow:
+        qa_dataset = StrategyQADataset()
+
+        def retrieve_passages(dataset, d):
+            for example in dataset:
+                question = example['question']
+                # embeded = retriever.embed_queries([question])
+                # embeded = embeded[0]
+                retrieved = retriever.retrieve(question, top_k=7)
+                # passages = [x.text for x in retrieved]
+                print(example)
+                d[question] = retrieved
+
+
+        result = {}
+        retrieve_passages(qa_dataset.train_set(), result)
+        retrieve_passages(qa_dataset.dev_set(), result)
+        print(result)
+
+        with open('strategy_retrieved_passages.json', 'w', encoding='utf-8') as f:
+            json.dump(result, f)
 
 res = retriever.retrieve('"who sings does he love me with reba')
 print(res)
